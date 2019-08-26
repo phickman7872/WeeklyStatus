90b1f38d36336ea1dc8716cb05c7413a9d463fcb
Phillip R. Hickman <phickman7872@gmail.com>
2019-08-26 07:02:19 -0400

    Added Core, Api and Web projects.

---
+++ Source/WeeklyStatus.Core/Models/TrackableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WeeklyStatus.Core.Models
+{
+    public class TrackableObject
+    {
+        public string UserCreated { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool IsDeleted { get; set; }
+        public string UserDeleted { get; set; }
+        public DateTime? DateDeleted { get; set; }
+        public string UserUpdated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public byte[] RowVersion { get; set; }
+    }
+}
---
+++ Source/WeeklyStatus.Core/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace WeeklyStatus.Core.Models
+{
+    public class User : TrackableObject
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PasswordSalt { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
